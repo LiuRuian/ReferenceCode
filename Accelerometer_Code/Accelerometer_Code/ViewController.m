e7bacd720e4a1f32729cb764b546a8dc7954f667
Ruian <duanrenhun@126.com>
2015-01-22 14:18:10 +0800

    加速计 iOS5之前

--- Accelerometer_Code/Accelerometer_Code/ViewController.m
+++ Accelerometer_Code/Accelerometer_Code/ViewController.m
@@ -6,9 +6,11 @@
 //  Copyright (c) 2015年 ruian. All rights reserved.
 //
 
+// 在iOS5之后被放弃
+
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIAccelerometerDelegate>
 
 @end
 
@@ -16,12 +18,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 1.设置加速计单例
+    UIAccelerometer *accelerometer = [UIAccelerometer sharedAccelerometer];
+    
+    // 2. 设置加速计代理
+    accelerometer.delegate = self;
+    
+    // 3.设置加速计的采集频率
+    accelerometer.updateInterval = 1 / 2.0;
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/*当采集到加速计数据就会调用此方法*/
+
+-(void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration
+{
+    NSLog(@"x= %f, y= %f, z= %f",acceleration.x,acceleration.y,acceleration.z);
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
+{
+    NSLog(@"size = %@,coordinator = %@",NSStringFromCGSize(size),coordinator);
+    
 }
 
 @end
