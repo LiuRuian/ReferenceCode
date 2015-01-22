69b98d21caee1e61c744da600deb5d481fcda6b6
Ruian <duanrenhun@126.com>
2015-01-22 14:22:46 +0800

    音效代码参考

--- AudioToolbox_Code/AudioToolbox_Code/ViewController.m
+++ AudioToolbox_Code/AudioToolbox_Code/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <AudioToolbox/AudioToolbox.h>   // 导入音效框架
 
 @interface ViewController ()
+
+@property (nonatomic, assign)SystemSoundID soundID;
+- (IBAction)playSoundAction:(UIButton *)sender;
 
 @end
 
@@ -16,12 +20,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    // 1.声明声音
+    SystemSoundID soundID = 0;
+    _soundID = soundID;
+    
+    // 2. 声音的路径
+    NSURL *soundURL = [[NSBundle mainBundle] URLForResource:@"alarmSound" withExtension:@"caf"];
+    
+    // 3.创建声音
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)(soundURL), &_soundID);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
+- (IBAction)playSoundAction:(UIButton *)sender {
+    
+    // 播放声音加振动
+    AudioServicesPlayAlertSound(_soundID);
+    // 播放振动
+    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+    
+}
 @end
