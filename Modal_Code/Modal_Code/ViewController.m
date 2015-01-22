00cced964ed097787cf5099d7b348ca0e0875914
Ruian <duanrenhun@126.com>
2015-01-22 14:20:32 +0800

    模态的几种方式

--- Modal_Code/Modal_Code/ViewController.m
+++ Modal_Code/Modal_Code/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ModalTableViewController.h"
 
 @interface ViewController ()
 
@@ -19,9 +20,36 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+/*! 模态的几种方式
+ typedef NS_ENUM(NSInteger, UIModalPresentationStyle) {
+ UIModalPresentationFullScreen = 0,
+ UIModalPresentationPageSheet NS_ENUM_AVAILABLE_IOS(3_2),
+ UIModalPresentationFormSheet NS_ENUM_AVAILABLE_IOS(3_2),
+ UIModalPresentationCurrentContext NS_ENUM_AVAILABLE_IOS(3_2),
+ UIModalPresentationCustom NS_ENUM_AVAILABLE_IOS(7_0),
+ UIModalPresentationOverFullScreen NS_ENUM_AVAILABLE_IOS(8_0),
+ UIModalPresentationOverCurrentContext NS_ENUM_AVAILABLE_IOS(8_0),
+ UIModalPresentationPopover NS_ENUM_AVAILABLE_IOS(8_0),
+ UIModalPresentationNone NS_ENUM_AVAILABLE_IOS(7_0) = -1,
+ };
+ */
+
+/*
+typedef NS_ENUM(NSInteger, UIModalTransitionStyle) {
+    UIModalTransitionStyleCoverVertical = 0,
+    UIModalTransitionStyleFlipHorizontal,
+    UIModalTransitionStyleCrossDissolve,
+    UIModalTransitionStylePartialCurl NS_ENUM_AVAILABLE_IOS(3_2),
+};
+ */
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    ModalTableViewController *modalVC = [[ModalTableViewController alloc] init];
+    
+    //modalVC.modalPresentationStyle = UIModalPresentationCurrentContext;
+    modalVC.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+    [self presentViewController:modalVC animated:YES completion:nil];
 }
 
 @end
